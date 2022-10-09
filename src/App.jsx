864ed8ab015e47255df7a5d162f0888cdd3d260f
Muhammad Dhiya Ulhaq <uhqdev@192.168.1.16>
2022-10-09 16:08:25 +0800

    add to nested component on card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
 import './index.css';
-import Button from './component/Button';
+import Button from './components/Button';
 import { BrandFacebook, BrandGithub, BrandTwitter } from 'tabler-icons-react';
+import Card from './components/Card';
 const App = () => {
     return (
         <>
-            <div className='bg-sky-800 grid place-content-center min-h-screen'>
+            <div className='bg-sky-100 antialiased text-slate-800 tracking-tight flex items-center justify-center min-h-screen'>
                 <div className='max-w-md w-full'>
                     <div className='flex gap-x-2'>
-                        <Button className='bg-sky-700' onClick={() => {console.log('Login')}} type='submit'>
-                            <BrandFacebook />
-                            Login
-                        </Button>
-                        <Button className='bg-purple-500' onClick={() => {console.log('Register')}} type='reset'>
-                            <BrandTwitter />
-                            Register
-                        </Button>
-                        <Button onClick={() => {console.log('Register')}} type='reset'>
-                            <BrandGithub/>
-                            Github
-                        </Button>
+                    </div>
+                    <div className='max-w-md items-center'>
+                        <Card>
+                            <Card.Title>
+                                Twitter
+                            </Card.Title>
+                            <Card.Body>
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adipiscing
+                            </Card.Body>
+                            <Card.Footer>
+                                <Button className='bg-purple-500' onClick={() => {console.log('Register')}} type='reset'>
+                                    <BrandTwitter />
+                                    Twitter
+                                </Button>
+                            </Card.Footer>
+                        </Card>
+
+                        <Card>
+                            <Card.Title>
+                                Facebook
+                            </Card.Title>
+                            <Card.Body>
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adipiscing
+                            </Card.Body>
+                            <Card.Footer>
+                                <Button className='bg-sky-700' onClick={() => {console.log('Login')}} type='submit'>
+                                    <BrandFacebook />
+                                    Facebook
+                                </Button>
+                            </Card.Footer>
+                        </Card>
                     </div>
                 </div>
             </div>
